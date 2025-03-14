1c219b282209af426c5948a28fd15b32c44ce60f
samitkumarpatel <samitkumarpatel@gmail.com>
2025-03-14 23:06:40 +0100

    fix

--- src/main/java/com/example/spring_oauth2_client/SpringOauth2ClientApplication.java
+++ src/main/java/com/example/spring_oauth2_client/SpringOauth2ClientApplication.java
@@ -1,16 +1,20 @@
 package com.example.spring_oauth2_client;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class SpringOauth2ClientApplication {
 
 	public static void main(String[] args) {
@@ -21,11 +25,20 @@ public class SpringOauth2ClientApplication {
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 class WebController {
+	final OAuth2AuthorizedClientService authorizedClientService;
+	final ClientRegistrationRepository clientRegistrationRepository;
 
-	@GetMapping("/token")
+	@GetMapping("/")
 	@ResponseBody
-	public String token(@RegisteredOAuth2AuthorizedClient("spring") OAuth2AuthorizedClient authorizedClient) {
-		return authorizedClient.getAccessToken().getTokenValue();
+	public String index(Authentication authentication) {
+		ClientRegistration registration =
+				this.clientRegistrationRepository.findByRegistrationId("clientone");
+
+		var token = registration.getClientSecret();
+		log.info("Client secret: {}", token);
+		return "index";
+
 	}
 }
