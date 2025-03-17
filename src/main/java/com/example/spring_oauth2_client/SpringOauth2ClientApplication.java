6185906bda8f46ec6030c73cb64fb47daf9830fd
samitkumarpatel <samitkumarpatel@gmail.com>
2025-03-17 15:11:50 +0000

    fix

--- src/main/java/com/example/spring_oauth2_client/SpringOauth2ClientApplication.java
+++ src/main/java/com/example/spring_oauth2_client/SpringOauth2ClientApplication.java
@@ -6,12 +6,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableWebSecurity
@@ -31,14 +36,24 @@ class WebController {
 	final ClientRegistrationRepository clientRegistrationRepository;
 
 	@GetMapping("/")
-	@ResponseBody
-	public String index(Authentication authentication) {
+	public String index() {
 		ClientRegistration registration =
 				this.clientRegistrationRepository.findByRegistrationId("clientone");
 
 		var token = registration.getClientSecret();
 		log.info("Client secret: {}", token);
-		return "index";
+		return "redirect:/index.html";
+
+	}
+
+	@GetMapping("/token")
+	@ResponseBody
+	public Map<String,String> token(@RegisteredOAuth2AuthorizedClient("clientone") OAuth2AuthorizedClient authorizedClient) {
+
+		return Map.of(
+				"access_token", authorizedClient.getAccessToken().getTokenValue(),
+				"refresh_token", Objects.nonNull(authorizedClient.getRefreshToken()) ? authorizedClient.getRefreshToken().getTokenValue() : ""
+		);
 
 	}
 }
