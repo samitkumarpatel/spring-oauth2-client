d049b4100f280e4cd52452117e8016066336473f
samitkumarpatel <samitkumarpatel@gmail.com>
2025-03-18 13:58:34 +0000

    more improvement

--- src/main/java/com/example/spring_oauth2_client/SpringOauth2ClientApplication.java
+++ src/main/java/com/example/spring_oauth2_client/SpringOauth2ClientApplication.java
@@ -1,22 +1,29 @@
 package com.example.spring_oauth2_client;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.Principal;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableWebSecurity
@@ -33,20 +40,19 @@ public class SpringOauth2ClientApplication {
 @Slf4j
 class WebController {
 	final OAuth2AuthorizedClientService authorizedClientService;
-	final ClientRegistrationRepository clientRegistrationRepository;
 
-	@GetMapping("/")
-	public String index() {
-		ClientRegistration registration =
-				this.clientRegistrationRepository.findByRegistrationId("clientone");
+	@GetMapping(value = "/")
+	public String index(HttpServletResponse response, Authentication authentication) {
 
-		var token = registration.getClientSecret();
+		var token = authorizedClientService.loadAuthorizedClient("clientone", authentication.getName())
+				.getAccessToken().getTokenValue();
 		log.info("Client secret: {}", token);
-		return "redirect:index.html";
-
+		Cookie cookie = new Cookie("access_token", token);
+		response.addCookie(cookie);
+		return "redirect:/index.html";
 	}
 
-	@GetMapping("/token")
+	@GetMapping("/access_token")
 	@ResponseBody
 	public Map<String,String> token(@RegisteredOAuth2AuthorizedClient("clientone") OAuth2AuthorizedClient authorizedClient) {
 
@@ -55,5 +61,11 @@ class WebController {
 				"refresh_token", Objects.nonNull(authorizedClient.getRefreshToken()) ? authorizedClient.getRefreshToken().getTokenValue() : ""
 		);
 
+	}
+
+	@GetMapping("/me")
+	@ResponseBody
+	public Map<String, Object> user(Authentication authentication) {
+		return Map.of();
 	}
 }
